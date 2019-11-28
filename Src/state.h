67ebddbcd2106bb74ca30cf4b9da8078762c9cb1
Ivan Johnson <git@IvanJohnson.net>
2019-11-28 12:09:06 -0600

    Implement state arguments

--- Src/state.h
+++ Src/state.h
@@ -10,6 +10,10 @@ typedef struct state (*stateFunction) (char **err, int *ret);
 struct state {
 	char *name;
 	stateFunction function;
+	void *args;
+
+	// if true, stateRun will free `args` before advancing to the next state.
+	bool shouldFreeArgs;
 };
 
 bool stateEqual(struct state state1, struct state state2);
@@ -17,24 +21,26 @@ bool stateValid(struct state state);
 int stateRun();
 
 
-#define STATE_EXIT ((struct state) { .name="exit", .function=NULL, })
+#define STATE_EXIT ((struct state) { .name="exit", .function=NULL, .args=NULL, .shouldFreeArgs=false, })
 
 struct state runError(char **err, int *ret);
-#define STATE_ERROR ((struct state) { .name="error", .function=runError, })
+#define STATE_ERROR ((struct state) { .name="error", .function=runError, .args=NULL, .shouldFreeArgs=false, })
 
 struct state runLowPower (char **err_msg, int *ret);
-#define STATE_LOW_POWER ((struct state) { .name="low-power", .function=runLowPower, })
+#define STATE_LOW_POWER ((struct state) { .name="low-power", .function=runLowPower, .args=NULL, .shouldFreeArgs=false, })
 
 struct state runCounting(char **err, int *ret);
-#define STATE_COUNTING ((struct state) { .name="counting", .function=runCounting, })
+#define STATE_COUNTING ((struct state) { .name="counting", .function=runCounting, .args=NULL, .shouldFreeArgs=false, })
 
 struct state runStarting(char **err, int *ret);
-#define STATE_STARTING ((struct state) { .name="starting", .function=runStarting, })
+
+#define STATE_STARTING ((struct state) { .name="starting", .function=runStarting, .args=NULL, .shouldFreeArgs=false, })
 
 struct state runRecording(char **err_msg, int *retStatus);
-#define STATE_RECORDING ((struct state) { .name="recording", .function=runRecording, })
+#define STATE_RECORDING ((struct state) { .name="recording", .function=runRecording, .args=NULL, .shouldFreeArgs=false, })
 
 static const struct state ALL_STATES[] = { STATE_EXIT, STATE_ERROR, STATE_LOW_POWER, STATE_STARTING, STATE_COUNTING, STATE_RECORDING };
+
 static const size_t NUM_STATES = sizeof(ALL_STATES) / sizeof(struct state);
 #define INITIAL_STATE STATE_LOW_POWER
 
